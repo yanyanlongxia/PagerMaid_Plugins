9b5fb5babc0d67e6b7af83a7de76d17fffe597ee
c3b2a <62082214+c3b2a@users.noreply.github.com>
2021-02-15 11:18:39 +0800

    Update yb-dl.py

--- yb-dl.py
+++ yb-dl.py
@@ -7,11 +7,11 @@ from re import compile as regex_compile
 from main import cmd, par, des, prefix_str
 from pyrogram import Client, filters
 
-cmd.append("ytdl")
+cmd.append("ybdl")
 par.append("<url>.")
 des.append("上传 Youtube、Bilibili 视频到 telegram")
 
-@Client.on_message(filters.me & filters.command("ytdl", list(prefix_str)))
+@Client.on_message(filters.me & filters.command("ybdl", list(prefix_str)))
 async def ybdl(client, message):
     url = " ".join(message.text.split(" ")[1:])
     reply = message.reply_to_message
@@ -23,18 +23,23 @@ async def ybdl(client, message):
         await message.edit("出错了呜呜呜 ~ 无效的参数。")
         return
 
+    bilibili_pattern = regex_compile(r"^(http(s)?://)?((w){3}.)?bilibili(\.com)?/.+")
     youtube_pattern = regex_compile(r"^(http(s)?://)?((w){3}.)?youtu(be|.be)?(\.com)?/.+")
     if youtube_pattern.match(url):
         if not await fetch_video(client, url, message.chat.id, reply_id):
             await message.edit("出错了呜呜呜 ~ 视频下载失败。")
         # await log(f"已拉取UTB视频，地址： {url}.")
         await message.edit("视频获取成功！")
-
+    if bilibili_pattern.match(url):
+        if not await fetch_video(client, url, message.chat.id, reply_id):
+            await message.edit("出错了呜呜呜 ~ 视频下载失败。")
+        # await log(f"已拉取 Bilibili 视频，地址： {url}.")
+        await message.edit("视频获取成功！")
 
 async def fetch_video(client, url, chat_id, reply_id):
     """ Extracts and uploads YouTube video. """
     youtube_dl_options = {
-        'format': 'bestvideo[height=720]+bestaudio/best',
+        'format': 'bestvideo+bestaudio/best',
         'outtmpl': "video.%(ext)s",
         'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
@@ -44,7 +49,7 @@ async def fetch_video(client, url, chat_id, reply_id):
     YoutubeDL(youtube_dl_options).download([url])
     if not exists("video.mp4"):
         return False
-    await client.send_video(
+    await client.send_document(
          chat_id,
          "video.mp4",
          reply_to_message_id=reply_id
